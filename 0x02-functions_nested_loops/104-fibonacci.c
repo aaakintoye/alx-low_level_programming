fe1556df4821799c22cd7d12bc74b96f16a37391
aaakintoye <aaakintoye2010@gmail.com>
2022-10-21 14:46:21 +0000

    a program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -31,7 +31,7 @@ int main(void)
 	while (i <= 98)
 	{
 		if (f1o > 0)
-			printf("%lu", f10);
+			printf("%lu", f1o);
 		initial0s = numLength(mx) - 1 - numLength(f1);
 		while (f1o > 0 && initial0s > 0)
 		{
